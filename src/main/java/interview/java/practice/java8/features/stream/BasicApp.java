97b450fe17bd4d7de27b7b3678084f76dcb3787a
Girase <tgirase@ptc.com>
2020-03-29 23:53:56 +0530

    New code for Stream API
    Added stream API learning, This will continue with advanced topics in
    future.

---
+++ src/main/java/interview/java/practice/java8/features/stream/BasicApp.java
@@ -0,0 +1,28 @@
+package interview.java.practice.java8.features.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class BasicApp {
+	public static void main(String[] args) {
+
+		List<Integer> integersList = new ArrayList<>();
+		for (int i = 0; i < 100; i++)
+			integersList.add(i);
+
+		printNumbers(integersList, (t) -> t > 90);
+
+		integersList.stream().filter((integ) -> integ > 90).filter(integ -> integ % 2 == 0)
+				.forEach(integ -> System.out.println(integ));
+
+	}
+
+	public static void printNumbers(List<Integer> list, Predicate<Integer> predicate) {
+		for (Integer integ : list) {
+			if (predicate.test(integ)) {
+				System.out.println(integ);
+			}
+		}
+	}
+}
---
+++ src/main/java/interview/java/practice/java8/features/stream/README.md
@@ -0,0 +1,7 @@
+#Stream
+	
+	Stream operations are divided into intermediate and terminal operations, and are combined to form 	streampipelines.
+	
+https://www.journaldev.com/2389/java-8-features-with-examples
+https://www.baeldung.com/java-8-new-features
+https://docs.oracle.com/javase/8/docs/api/java/util/stream/package-summary.html
